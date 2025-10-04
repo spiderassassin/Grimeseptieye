a294e3050b865aad7ff91086e4f5f236d8a7a19b
Dave Goel <davegoel29@gmail.com>
2025-10-04 16:32:39 -0600

    dialogue system

---
+++ GrimJack/Assets/DialogueCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room1 : MonoBehaviour
+{
+    public DialogueTrigger trigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        trigger.TriggerDialogue();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
