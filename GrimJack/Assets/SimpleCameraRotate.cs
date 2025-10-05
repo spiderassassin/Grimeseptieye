f5b2922f52e03e2da9820036aee25fcfcaae0448
Dave Goel <davegoel29@gmail.com>
2025-10-05 15:09:20 -0600

    enemy animation

---
+++ GrimJack/Assets/SimpleCameraRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCameraRotate : MonoBehaviour
+{
+    public Transform target;
+    public float rotationSpeed = 120f;
+    public Vector2 pitchLimits = new Vector2(-30f, 60f);
+    private float yaw;
+    private float pitch;
+
+    void LateUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        yaw += mouseX * rotationSpeed * Time.deltaTime;
+        pitch -= mouseY * rotationSpeed * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, pitchLimits.x, pitchLimits.y);
+
+        transform.position = target.position;
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+    }
+}
---
+++ GrimJack/Assets/attackCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackCollision : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            
+            if(PlayerController.Instance.isAttacking == true)
+            {
+                print("aaa");
+                other.gameObject.GetComponent<EnemyBehavior>().TakeDamage(30f);
+                PlayerController.Instance.isAttacking = false;
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
