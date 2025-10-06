e1f99c28e213e51bd81cab1d9cc503791376d750
HaleyJPEG <hskjersv@ualberta.ca>
2025-10-05 21:34:44 -0600

    WOOOO

--- GrimJack/Assets/SceneTransition.cs
+++ GrimJack/Assets/SceneTransition.cs
@@ -8,17 +8,27 @@ public class SceneTransition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Play()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    { 
+        Application.Quit();
+     }
 }
